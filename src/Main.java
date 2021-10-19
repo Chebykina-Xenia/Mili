58401de0f2a6df2103cc9ec4c2bce226d3235bae
chebykina <chebykina.xenia@gmail.com>
2021-10-19 11:02:59 +0300

    Доработка ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-        int ticket_price = 2_000; //стоимость билета
+        int ticketPrice = 2_000; //стоимость билета
         int bonus = 20;          //рублей за 1 милю
-
         //Расчитываем мили
-        int milii = ticket_price / bonus;
-
+        int milii = ticketPrice / bonus;
         System.out.println(milii);
     }
 }
